31f4a7e28b1df4c020562589e844fe1b71ee88c9
Palkor741 <Nurahmanjr741@gmail.com>
2026-08-07 08:45:22 +0700

    add if pinfo

--- drivers/power/supply/mediatek/charger/mtk_charger.c
+++ drivers/power/supply/mediatek/charger/mtk_charger.c
@@ -854,6 +854,9 @@ void charger_manager_set_prop_system_temp_level(int temp_level)
 
 	if (pinfo == NULL)
 		return ;
+
+        if (pinfo->usb_psy == NULL)
+                return ;
 	pcba_to_thermal = get_huaqin_pcba_config();
 #if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
 	if (pcba_to_thermal == PCBA_J19_MP_CN)
